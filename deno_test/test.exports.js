89971e7b1bfa920a39562661b10dfa91369629c0
tokha-it <tokha@langivi.technology>
2023-01-17 12:58:29 +0200

    add test exports

---
+++ deno_test/test.exports.js
@@ -0,0 +1,45 @@
+import { assertEquals, assertThrows } from "https://deno.land/std@0.143.0/testing/asserts.ts";
+import { twig } from "../src/twig.js";
+
+Deno.test("Twig.js Exports __express ->", async (t) => {
+    await t.step('should return a string (and not a String)', async () => {
+        twig.__express('./templates/test.twig', {
+            settings: {
+                'twig options': {
+                    autoescape: 'html'
+                }
+            }
+        }, (err, response) => {
+            assertEquals((err === null), true);
+            const responseType = (typeof response);
+            assertEquals(responseType, 'string');
+            
+        });
+    })
+
+    await t.step('', async () => {
+        twig.__express('./templates/test-async.twig', {
+            settings: {
+                'twig options': {
+                    allowAsync: true
+                }
+            },
+            /* eslint-disable-next-line camelcase */
+            hello_world() {
+                return Promise.resolve('hello world');
+            }
+        }, (err, response) => {
+            if (err) {
+                return;
+            }
+
+            try {
+                const responseType = (typeof response);
+                assertEquals(responseType, 'string');
+                assertEquals(response, 'hello world\n');
+            } catch (error) {
+               console.log(error);
+            }
+        });
+    })
+})
